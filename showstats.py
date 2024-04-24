b9f9754c5e2995ccf51ec9a56b6d1ef8a857bded
Colin Gallagher <39956444+colin-gall@users.noreply.github.com>
2024-04-24 15:05:25 -0400

    Update showstats.py

--- showstats.py
+++ showstats.py
@@ -8,13 +8,22 @@ import requests
 import pandas as pd
 from tqdm.auto import tqdm
 
+__repository__ = 'showstats'
+__license__ = "MIT"
+__version__ = "0.0.1"
+__location__ = "https://github.com/colin-gall/showstats/"
+__author__ = "colingall"
+__email__ = "colingall@pm.me"
+
 def download_json(page):
+    '''Handshakes with API address provided on MLB The Show 24 community market API docs and returns content as JSON object.'''
     url = f"https://mlb24.theshow.com/apis/items.json?type=mlb_card&page={page}"
     page = requests.get(url)
     if page.status_code == 200:
         return page.json()
 
 def download_items():
+    '''Total page count is determined, JSON data is gathered for each page from API and stored in cumulative list at the end of each iteration, and finally data is parsed, organized, and returned as a Pandas dataframe.'''
     json_data = download_json(1)
     num_pages = int(json_data['total_pages'])
     all_items = []
@@ -29,6 +38,7 @@ def download_items():
     return df
 
 def download_csv(df, filename=None):
+    '''Attempts to convert dataframe containing JSON data to Excel workbook file saved to current working directory, and creates a 'pkl' (i.e. pickle) file if it encounters any issues during the process to prevent data loss.''' 
     try:
         if filename is None:
             filename = f"mlbtheshow_data_{date.today().year}{date.today().month}{date.today().day}.xlsx"
@@ -40,6 +50,7 @@ def download_csv(df, filename=None):
         df.to_pickle(filename)
 
 def execute():
+    '''Runtime core functionality for script execution or if called as main module.'''
     df = download_items()
     download_csv(df)
     print('_____________\n|   Done!   |\n*-----------*')
